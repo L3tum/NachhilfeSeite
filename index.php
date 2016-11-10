4b7f78aac65bf4388f2ac95a576d9a19eef29ac1
L3tum <tomn.pauly@gmail.com>
2016-11-10 18:15:12 +0100

    Video2

--- index.php
+++ index.php
@@ -21,10 +21,7 @@ session_start();
         <h1 style="color: deeppink;">HEEEEEY</h1>
         <h2>Spectacular Mountain</h2>
         <p><a href="https://www.donaldjtrump.com/">Visit Gymnasium-Lohmar.com!</a></p>
-        <video width="320" height="240" controls>
-            <source src="https://www.youtube.com/watch?v=YEwlfJdTldw" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/YEwlfJdTldw?autoplay=1" frameborder="0" allowfullscreen></iframe>
     </body>
     
     <form action="index.php" method="post">
