4193c86af3ec380bf8d7ab348a08a0833066ee0c
L3tum <tomn.pauly@gmail.com>
2016-11-10 19:10:58 +0100

    main site

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
 
 
 <body style="background-color: darkgray">
-<div style="position: fixed; z-index: -99; width: 100%; height: 100%">
+<div style="position: fixed; z-index: -99; width: 100%; height: 100%; left: 0%; top: 0%">
     <iframe frameborder="0" height="100%" width="100%"
             src="https://youtube.com/embed/YEwlfJdTldw?autoplay=1&controls=0&showinfo=0&autohide=1&vq=hd720&loop=1&playlist=YEwlfJdTldw">
     </iframe>
