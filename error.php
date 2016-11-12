69f3424408b3f336ec0901ff8f08b53eb8b74a23
L3tum <tomn.pauly@gmail.com>
2016-11-12 08:48:28 +0100

    error page, -babel

---
+++ error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom
+ * Date: 12.11.2016
+ * Time: 08:01
+ */
+session_start();
+?>
+
+<div style="position: fixed; z-index: -99; width: 100%; height: 100%; left: 0%; top: 0%; margin: 0">
+    <iframe frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" allowfullscreen
+            src="https://youtube.com/embed/YEwlfJdTldw?autoplay=1&controls=0&showinfo=0&autohide=1&vq=hd720&loop=1&playlist=YEwlfJdTldw">
+    </iframe>
+</div>
--- index.php
+++ index.php
@@ -15,13 +15,7 @@ session_start();
 
 
 <body style="background-color: darkgray">
-<div style="position: fixed; z-index: -99; width: 100%; height: 100%; left: 0%; top: 0%; margin: 0">
-    <iframe frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" allowfullscreen
-            src="https://youtube.com/embed/YEwlfJdTldw?autoplay=1&controls=0&showinfo=0&autohide=1&vq=hd720&loop=1&playlist=YEwlfJdTldw">
-    </iframe>
-</div>
 <div id="root" class="container" align="left">
-    <script src="index.js"></script>
     <h1 style="color: deeppink">HEEEEEY</h1>
     <h2 style="background-color: whitesmoke; width: 6%">Spectacular Mountain</h2>
     <p><a style="background-color: whitesmoke" href="https://www.donaldjtrump.com/">Visit Gymnasium-Lohmar.com!</a></p>
