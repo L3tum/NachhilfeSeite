5c908ebf5426af151e3e094d4d64dbb92e69b0f1
L3tum <tomn.pauly@googlemail.com>
2016-12-07 11:58:45 +0100

    PUSH

--- index.php
+++ index.php
@@ -1,79 +1,203 @@
 <?php
+
 session_start();
+
+//Hole die Datenbankverbidnung
+require_once ("dbopen.php");
+//integriere die DAOUser
+require_once ("DAO/DAOUser.php");
+//integriere die Klasse CUser
+require_once ("Class/CUser.php");
+//Hier PHP Skript
+
+//neues Objekt der Klasse DAOUSer, Übergabe der Verbindung aus der dbopen.php Datei
+$daoUser = new DAOUser($connection);
+
+//BEISPIEL
+$meinBenutzer = $daoUser->getUserById(1); //Rufe die Methode getUserById(xx) des Objekts DAOUser auf -> "Gib mir den Benutzer mit der ID xx"
+//meinBenutzer ist nun "null" oder beinhaltet ein Objekt der Klasse CUser
+
+
+//Die eigentliche Anmeldung!
+//Prüfe ob Formular abgesendet wurde
+if(isset($_POST['action']) && isset($_POST['benutzer']) && isset($_POST['password'])) {
+    $meinBenutzer = $daoUser->getUserByZugangsdaten($_POST['benutzer'], $_POST['password']);
+}
+
+
+
+
+if(is_object($meinBenutzer)) {
+    $meinBenutzer->setSession($session_id);
+    //Wenn meinBenutzer ein Objekt ist, also nicht "null" ist, melde ihn in der Datenbank an
+    if($daoUser->anmelden($meinBenutzer)) { //Anmeldung erfolgreich
+
+    } else { //Anmeldung war nicht erfolgreich
+
+    }
+} else {
+    //meinBenutzer ist kein Objekt, also existiert kein Benutzer mit der id
+    echo "Kann den Benutzer nicht finden. Benutezrname/Kennwort korrekt?";
+}
+
 ?>
 
 <!DOCTYPE html>
-<html class="no-js" lang="en">
+<html lang="en">
+
 <head>
-    <meta charset="utf-8"/>
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Nachhilfe</title>
-    <link rel="stylesheet" href="css/foundation.css"/>
-    <link rel="stylesheet" href="CSS/testing.css"/>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="vendor/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 
+<body>
 
-<body style="background-color: darkgray">
-<div id="root" class="container" align="left">
-    <h1 style="color: deeppink">HEEEEEY</h1>
-    <h2 style="background-color: whitesmoke; width: 6%">Spectacular Mountain</h2>
-    <p><a style="background-color: whitesmoke" href="https://www.donaldjtrump.com/">Visit Gymnasium-Lohmar.com!</a></p>
+<div id="wrapper">
 
-    <!--<iframe width="560" height="315" src="https://www.youtube.com/embed/YEwlfJdTldw?autoplay=1" frameborder="0" allowfullscreen></iframe> -->
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">Gymnasium Lohmar Oben links</a>
+        </div>
+        <!-- /.navbar-header -->
 
-    <div class="form-group">
-        <form action="index.php" method="post">
-            <label style="background-color: whitesmoke" for="vorname">Vorname:</label>
-            <input style="background-color: whitesmoke" class="form-control" type="text" name="vorname"/><br/>
+        <!--Hauptnavigation-->
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li>
+                        <a href="http://gymnasium-lohmar.org"><i class="fa fa-dashboard fa-fw"></i>Gymnasium Lohmar</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="#">Second Level Item</a>
+                            </li>
+                            <li>
+                                <a href="#">Second Level Item</a>
+                            </li>
+                            <li>
+                                <a href="#">Third Level <span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Third Level Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Item</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Item</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-third-level -->
+                            </li>
+                        </ul>
+                        <!-- /.nav-second-level -->
+                    </li>
+                </ul>
+            </div>
+            <!-- /.sidebar-collapse -->
+        </div>
+        <!-- /.navbar-static-side -->
+    </nav>
 
-            <label style="background-color: whitesmoke" for="vorname">Name:</label>
-            <input style="background-color: whitesmoke" class="form-control" type="text" name="name"/><br/>
+    <!-- AB HIER INHALT-->
 
-            <label style="background-color: whitesmoke" for="vorname">Passwort:</label>
-            <input style="background-color: whitesmoke" class="form-control" type="password" name="passwort"/><br/>
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Dashboard</h1>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form"action="" method="POST">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Benutzername" name="benutzer" type="text" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
 
-            <label style="background-color: whitesmoke" for="vorname">Passwort bestätigen:</label>
-            <input style="background-color: whitesmoke" class="form-control" type="password" name="passwortBestaetigt"/><br/>
-
-            <label style="background-color: whitesmoke" for="vorname">Telefonnummer:</label>
-            <input style="background-color: whitesmoke" class="form-control" type="tel" name="telefon"/><br/>
-
-            <label style="background-color: whitesmoke" for="vorname">Email:</label>
-            <input style="background-color: whitesmoke" class="form-control" type="email" name="email"/><br/>
-
-            <label style="background-color: whitesmoke" for="vorname">Männlich:</label>
-            <input style="background-color: whitesmoke" class="form-control" type="checkbox" name="maennlich"/><br/>
-            <input style="background-color: whitesmoke" class="form-control" type="submit" name="submit"
-                   value="Fuck off"/>
-        </form>
+                                <input type="submit" name="action" value="anmelden">
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
     </div>
+    <!-- /#page-wrapper -->
+
 </div>
-<div>
-    <script src="https://unpkg.com/react@15/dist/react.min.js"></script>
-    <script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
-    <script src="js/vendor/jquery.min.js"></script>
-    <script src="js/vendor/what-input.min.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>$(document).foundation();</script>
-</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="vendor/metisMenu/metisMenu.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="vendor/raphael/raphael.min.js"></script>
+<script src="vendor/morrisjs/morris.min.js"></script>
+<script src="data/morris-data.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="dist/js/sb-admin-2.js"></script>
+
 </body>
+
 </html>
-
-<?php
-
-if (isset($_POST['submit'], $_POST['vorname'], $_POST['name'], $_POST['email'], $_POST['passwort'], $_POST['passwortBestaetigt'], $_POST['tel']) && !empty($_POST['vorname']) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['passwort'] && $_POST['passwortBestaetigt'] && !empty($_POST['tel']))) {
-    $passwortverschlusselt = crypt($_POST['passwort'], "WAS IST DAS FUER EINE SCHEISSE DU ARSCHLOCH FUCK OFF");
-    $bestaetigtPasswortVerschluesselt = crypt($_POST['passwortBestaetigt'], "WAS IST DAS FUER EINE SCHEISSE DU ARSCHLOCH FUCK OFF");
-
-    echo $passwortverschlusselt . "<br/>" . $bestaetigtPasswortVerschluesselt . "<br/>";
-    if ($passwortverschlusselt !== $bestaetigtPasswortVerschluesselt) {
-        echo "FEHLER DU ARSCH<br/>";
-    }
-    mail($_POST['email'], $_POST['name'], $_POST['vorname'] . " " . $_POST['email'] . " " . $_POST['passwort'] . " " . $_POST['tel']);
-    echo "Crypt: " . crypt($_POST['vorname'] . $_POST['name'] . $_POST['email'] . $_POST['passwort'] . $_POST['passwortBestaetigt'] . $_POST['tel'], $_POST['email'] . session_id());
-    var_dump($passwortverschlusselt, $bestaetigtPasswortVerschluesselt);
-} else if (isset($_POST['submit'])) {
-    echo "FEHLER DU ARSCH<br/>";
-}
-?>
