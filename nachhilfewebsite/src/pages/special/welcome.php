52e8fd5a4ce21bf3ee09a6339f34677af14f4784
BitteWenden <goeller.tim@googlemail.com>
2016-12-16 16:53:10 +0100

    Welcome page improvements

--- nachhilfewebsite/src/pages/special/welcome.php
+++ nachhilfewebsite/src/pages/special/welcome.php
@@ -13,41 +13,30 @@ layout: special
 <form data-abide novalidate>
     <div class="row">
 
-        <div class="small-12 columns">
+        <div class="small-12 medium-6 columns small-centered">
             <label>Vorname
                 <input type="text" placeholder="Max" required pattern="^[a-zA-ZÄÖÜäöüß]{1,25}$">
-        <span class="form-error">
-          Der Vorname darf nicht leer sein und nur aus Bustaben bestehen.
-        </span>
+                <span class="form-error">
+                    Der Vorname darf nicht leer sein und nur aus Bustaben bestehen.
+                </span>
             </label>
-        </div>
 
-        <div class="small-12 columns">
             <label>Nachname
                 <input type="text" placeholder="Mustermann" required pattern="^[a-zA-ZÄÖÜäöüß]{1,25}$">
-        <span class="form-error">
-          Der Nachname darf nicht leer sein und nur aus Bustaben bestehen.
-        </span>
+                <span class="form-error">
+                    Der Nachname darf nicht leer sein und nur aus Bustaben bestehen.
+                </span>
             </label>
-        </div>
 
-        <div class="small-12 columns">
             <label>Passwort
                 <input type="password" required>
-        <span class="form-error">
-          Das Passwortfeld darf nicht leer sein.
-        </span>
+                <span class="form-error">
+                    Das Passwortfeld darf nicht leer sein.
+                </span>
             </label>
+
+            <button class="button" type="submit" value="Submit">Submit</button>
         </div>
 
 
-
-    </div>
-
-    <div class="row">
-        <fieldset class="large-6 columns">
-            <button class="button" type="submit" value="Submit">Submit</button>
-        </fieldset>
-
-    </div>
 </form>
