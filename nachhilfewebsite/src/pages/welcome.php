ef4ff0ffba7cc21de6494751efb7b9228ee41bf8
BitteWenden <goeller.tim@googlemail.com>
2016-12-16 15:56:33 +0100

    Form an style for welcome page

--- nachhilfewebsite/src/pages/welcome.php
+++ nachhilfewebsite/src/pages/welcome.php
@@ -1,4 +1,53 @@
 ---
 layout: special
 ---
-<h1>Willkommen bei der Nachhilfe des Gymnasium Lohmar !</h1>
+<div class="row">
+    <div class="small-12 columns">
+        <h1 class="text-center">Willkommen bei der Nachhilfe des Gymnasiums Lohmar !</h1>
+    </div>
+</div>
+<div class="row">
+
+</div>
+
+<form data-abide novalidate>
+    <div class="row">
+
+        <div class="small-12 columns">
+            <label>Vorname
+                <input type="text" placeholder="Max" required pattern="^[a-zA-ZÄÖÜäöüß]{1,25}$">
+        <span class="form-error">
+          Der Vorname darf nicht leer sein und nur aus Bustaben bestehen.
+        </span>
+            </label>
+        </div>
+
+        <div class="small-12 columns">
+            <label>Nachname
+                <input type="text" placeholder="Mustermann" required pattern="^[a-zA-ZÄÖÜäöüß]{1,25}$">
+        <span class="form-error">
+          Der Nachname darf nicht leer sein und nur aus Bustaben bestehen.
+        </span>
+            </label>
+        </div>
+
+        <div class="small-12 columns">
+            <label>Passwort
+                <input type="password" required>
+        <span class="form-error">
+          Das Passwortfeld darf nicht leer sein.
+        </span>
+            </label>
+        </div>
+
+
+
+    </div>
+
+    <div class="row">
+        <fieldset class="large-6 columns">
+            <button class="button" type="submit" value="Submit">Submit</button>
+        </fieldset>
+
+    </div>
+</form>
