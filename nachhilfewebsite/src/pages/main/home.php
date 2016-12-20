f445c528db86bfeb31f10272c92539c8e0f8857f
BitteWenden <goeller.tim@googlemail.com>
2016-12-20 19:41:32 +0100

    data tags + home screen improvements

--- nachhilfewebsite/src/pages/main/home.php
+++ nachhilfewebsite/src/pages/main/home.php
@@ -5,12 +5,19 @@
         <div class="row">
             <div class="small-6 columns">
                 <h2>Nachhilfe</h2>
-                <p>Dein Name: <?php echo Benutzer::get_logged_in_user()->vorname . " " . Benutzer::get_logged_in_user()->name ?></p>
-                <p>Deine Rolle: <?php echo Benutzer::get_logged_in_user()->get_role() ?></p>
+                <div class="data-label">
+                    <p>Dein Name: <?php echo Benutzer::get_logged_in_user()->vorname . " " . Benutzer::get_logged_in_user()->name ?></p>
+                </div>
+
+                <div class="data-label">
+                    <p>Deine Rolle: <?php echo Benutzer::get_logged_in_user()->get_role() ?></p>
+                </div>
+
+
             </div>
 
             <div class="small-6 columns" style="margin-top:15px">
-                <img src="http://gymnasium-lohmar.org/images/Logo/Schullogo_Homepage.png"/>
+                <img class="float-center" src="http://gymnasium-lohmar.org/images/Logo/Schullogo_Homepage.png"/>
             </div>
         </div>
 
@@ -21,10 +28,9 @@
     <div class="small-12 smallmedium-12 medium-6 columns" data-equalizer-watch>
 
         <h2>Willkommen!</h2>
-        <p>Commodi sit esse voluptatem atque eum commodi quis. Omnis consequatur enim porro. Voluptate non ut quidem eaque dicta. Voluptatem labore cum autem. Deserunt et vel suscipit qui velit molestiae enim molestiae.
-            Consectetur dolorem aliquam dolorum. Quibusdam nesciunt sed et consequatur adipisci. Rerum sit aut ratione ea incidunt. Corrupti id pariatur praesentium sed. Voluptatem et non at est odit ducimus qui nihil. Eum hic repudiandae explicabo perferendis sapiente.
-            Soluta voluptate a nobis hic cum. Voluptatem alias reiciendis sed sit. Quibusdam autem quaerat expedita tempore qui dolores dolores optio. In corporis aspernatur eius qui reiciendis voluptatem. Et quod sed vitae blanditiis est. Aut officia est molestiae.
-            Eum molestiae minus est et totam. Enim praesentium id incidunt repellat esse voluptatem autem. Accusamus deserunt odio quia quam. Sunt consequuntur quaerat soluta hic quisquam vero rerum.
-            Exercitationem architecto laborum totam asperiores quis dolores ea. Voluptas atque voluptatem assumenda perferendis quis doloremque similique odio. Nemo et natus harum ut modi perspiciatis hic. In unde est rerum et error qui aspernatur. Sed temporibus officia nemo.</p>
+        <p>Willkommen bei der Nachhilfe des Gymnasiums Lohmar! Auf dieser Seite wird die Organsiation der Nachhilfe geregelt.</p>
+        <p>Bla bla. Bla bla bla. Bla bla bla bla.</p>
+        <p>Ich bin ein Absatz und weiß nicht, was ich hier soll.</p>
+        <p>Ich bin auch ein Absatz und will hier raus.</p>
     </div>
 </div>
