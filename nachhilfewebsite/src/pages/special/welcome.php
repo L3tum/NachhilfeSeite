fed5d00964d0bde23ebe953b46ef3f7b9b3789b5
L3tum <tomn.pauly@gmail.com>
2017-06-27 15:20:43 +0200

    Fixed

--- nachhilfewebsite/src/pages/special/welcome.php
+++ nachhilfewebsite/src/pages/special/welcome.php
@@ -8,8 +8,10 @@ layout: special
 </div>
 <div class="row">
     <div class="small-12-centered columns">
+        <!--
         <div id="fullpage">
             <div class="fp-section">
+            -->
                 <form data-abide novalidate id="login-form" method="post">
                     <div class="row">
                         <div class="small-12 medium-6 columns small-centered">
@@ -42,47 +44,49 @@ layout: special
                     </div>
                 </form>
                 <br>
-            </div>
-            <div class="fp-section">
-                <div class="portfolio-resume-scrolling-container row">
-                    <div class="columns small-12 medium-5 portfolio-resume-overview portfolio-image-one">
-                    </div>
-                    <div class="columns small-12 medium-7 portfolio-resume-scrolling">
-                        <h3>Sichere Verbindungen</h3>
-                        <ul class="portfolio-resume-side-list">
-                            <li>Jede Verbindung ist mit SSL verschlüsselt.</li>
-                            <li>Durch diese Verschlüsselung kommt wirklich niemand an deine Daten.</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="fp-section">
-                <div class="portfolio-resume-scrolling-container row">
-                    <div class="columns small-12 medium-5 portfolio-resume-overview portfolio-image-two">
-                    </div>
-                    <div class="columns small-12 medium-7 portfolio-resume-scrolling">
-                        <h3>Verschlüsselung</h3>
-                        <ul class="portfolio-resume-side-list">
-                            <li>Alle deine wichtigen Daten werden von uns verschlüsselt.</li>
-                            <li>Dein Passwort wird sogar zwei mal verschlüsselt, um dir die maximale Sicherheit zu bieten.</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="fp-section">
-                <div class="portfolio-resume-scrolling-container row">
-                    <div class="columns small-12 medium-5 portfolio-resume-overview portfolio-image-three">
-                    </div>
-                    <div class="columns small-12 medium-7 portfolio-resume-scrolling">
-                        <h3>Vernetzung</h3>
-                        <ul class="portfolio-resume-side-list">
-                            <li>Die gesamte Welt ist mittlerweile vernetzt, und so wollen wir auch unsere Benutzer vernetzen.</li>
-                            <li> Deshalb können alle Aktionen, inklusive des Chats, in Echtzeit auf dieser Seite benutzt werden!</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+        <!--
+</div>
+<div class="fp-section">
+    <div class="portfolio-resume-scrolling-container row">
+        <div class="columns small-12 medium-5 portfolio-resume-overview portfolio-image-one">
         </div>
+        <div class="columns small-12 medium-7 portfolio-resume-scrolling">
+            <h3>Sichere Verbindungen</h3>
+            <ul class="portfolio-resume-side-list">
+                <li>Jede Verbindung ist mit SSL verschlüsselt.</li>
+                <li>Durch diese Verschlüsselung kommt wirklich niemand an deine Daten.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="fp-section">
+    <div class="portfolio-resume-scrolling-container row">
+        <div class="columns small-12 medium-5 portfolio-resume-overview portfolio-image-two">
+        </div>
+        <div class="columns small-12 medium-7 portfolio-resume-scrolling">
+            <h3>Verschlüsselung</h3>
+            <ul class="portfolio-resume-side-list">
+                <li>Alle deine wichtigen Daten werden von uns verschlüsselt.</li>
+                <li>Dein Passwort wird sogar zwei mal verschlüsselt, um dir die maximale Sicherheit zu bieten.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="fp-section">
+    <div class="portfolio-resume-scrolling-container row">
+        <div class="columns small-12 medium-5 portfolio-resume-overview portfolio-image-three">
+        </div>
+        <div class="columns small-12 medium-7 portfolio-resume-scrolling">
+            <h3>Vernetzung</h3>
+            <ul class="portfolio-resume-side-list">
+                <li>Die gesamte Welt ist mittlerweile vernetzt, und so wollen wir auch unsere Benutzer vernetzen.</li>
+                <li> Deshalb können alle Aktionen, inklusive des Chats, in Echtzeit auf dieser Seite benutzt werden!</li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+-->
     </div>
 </div>
 
